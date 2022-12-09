3b408f8df603c7f66fb4012dc4ffe73b73471278
mathewoyeniyi <oyeniyi.matthew@gmail.com>
2022-12-09 21:29:07 +0100

    Commit Task

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    for k, v in sorted(a_dictionary.items()):
-        print(k, end=": ")
-        print(v)
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return (a_dictionary)

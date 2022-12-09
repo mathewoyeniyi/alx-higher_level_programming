d6158978a121a4fdb3c717c0873460126bbc621d
mathewoyeniyi <oyeniyi.matthew@gmail.com>
2022-12-09 21:08:58 +0100

    Commit Task

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,3 @@
-#def square_matrix_simple(matrix=[]):
- #   """Compute the square value of all integers of a matrix."""
-  #  return ([list(map(lambda x: x * x, row)) for rowin matrix])
-
-  #!/usr/bin/python3
-# 0-square_matrix_simple.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    """Compute the square value of all integers of a matrix."""
-    return ([list(map(lambda x: x * x, row)) for row in matrix])
+    return ([list(map(lambda x: x ** 2, i))for i in matrix])
